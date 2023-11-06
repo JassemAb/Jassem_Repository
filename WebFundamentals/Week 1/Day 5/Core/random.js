d6af7ebc35e4efecfca51d16c82db9d346a2130f
JassemAb <jikoabidi@gmail.com>
2023-11-06 17:11:30 +0100

    commit

--- WebFundamentals/Week 1/Day 5/Core/random.js
+++ WebFundamentals/Week 1/Day 5/Core/random.js
@@ -1,44 +1,20 @@
-var floor = Math.floor(1.8);
-var ceiling = Math.ceil( Math.PI );
-var random = Math.random();
-    
-console.log(floor);
-console.log(ceiling);
-console.log(random);
-// 1 - Dice Roller
-function d6() {
-    var roll = Math.random();
-    //console.log("Ceil == >" ,roll * 6);
-    //console.log("Floor ==>" , roll * 7);
-    var randomValue = Math.ceil(roll * 6);
-    console.log("Roll = ", roll);
-    console.log("Random Value =" , randomValue);
-    return roll;
+function login(element){
+    if (element.innerText == "login") {
+        element.innerText = "Logout";
+    }
+    else{
+        element.innerText = "login";
+    }
 }
-var playerRoll = d6();
-console.log("The player rolled: " + playerRoll);
 
-// 2 - Consult the Oracle
-var lifesAnswers = [
-    "It is certain.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Yes – definitely.",
-    "You may rely on it.",
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful."
-];
-d6()
+
+function remove(button){
+    button.remove();
+}
+
+
+
+
+function alerte(){
+    alert("This page says Ninja was liked");
+}
