11e1268847ece0c368da51d8a7a98089d7b17c3b
JassemAb <jikoabidi@gmail.com>
2023-11-15 21:55:33 +0100

    commit

--- Python/Week 1/Day 2/Core/BankAccount.py
+++ Python/Week 1/Day 2/Core/BankAccount.py
@@ -1,22 +1,35 @@
-class BankAcccount:
+class BankAccount:
     #A class to represent a banc account.
 
     def __init__(self, balance=0, interest_rate=0.01):
         self.balance = balance
         self.interest_rate = interest_rate
 
-        def deposit(self, amount):
-            self.balance += amount
-
-        def withdraw(self, amount):
-            if self.balance >=amount:
-                self.balance -=amount
-            else:
-                print("insufficient funds")
-                self.balance -=5
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        if self.balance >=amount:
+            self.balance -=amount
+            return self
+        else:
+            print("insufficient funds")
+            self.balance -=5
+            return self
     def display_account_info(self):
         print(f"balance: {self.balance}")
+        return None
 
     def yield_interest(self):
-        if self.balance>0:
-            self.balance+=self.balance * self.interest_rate
+        
+        self.balance+=self.balance * self.interest_rate
+        return self
+
+
+account1=BankAccount()
+account1.deposit(100).deposit(200).deposit(200).withdraw(200).yield_interest().display_account_info()
+account1.display_account_info()
+
+account2=BankAccount()
+account2.deposit(160).deposit(140).withdraw(50).withdraw(10).withdraw(15).withdraw(20).yield_interest().display_account_info()
+account2.display_account_info()
