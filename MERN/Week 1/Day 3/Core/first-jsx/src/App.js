f53685245c3aad086a8f482951a7fa17b60cad36
JassemAb <jikoabidi@gmail.com>
2024-04-11 16:27:41 +0100

    commit

---
+++ MERN/Week 1/Day 3/Core/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import AnotherComponent from "./components/AnotherComponent"
+
+function App() {
+  return (
+    <div className="App">
+    
+      
+      <h1>Hello Dojo</h1>
+
+      <AnotherComponent />
+    
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ MERN/Week 1/Day 3/Core/first-jsx/src/components/AnotherComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const AnotherComponent = () => {
+ return(
+ <div>
+   
+  <h3>Things I need to do:</h3>
+  <p>*Learn React</p>
+  <p>*Climb Mt. Everest</p>
+  <p>*Run a marathon</p>
+  <p>*Feed the dogs</p>
+ </div>
+ )
+}
+
+export default AnotherComponent;
