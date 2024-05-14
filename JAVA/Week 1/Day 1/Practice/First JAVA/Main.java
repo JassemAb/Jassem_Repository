4e57261cd6c86d47f7ea62bde0f9ce22a204855c
JassemAb <jikoabidi@gmail.com>
2024-05-14 13:39:05 +0100

    commit

---
+++ JAVA/Week 1/Day 1/Practice/First JAVA/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+ public static void main (String[]args){
+ String name  = "Jassem";
+ int age  = 20;
+ String hometown  = "Gassrin";
+  System.out.println("My Name Is " + name);
+  System.out.printf("I am %s years old \n" ,age);
+  System.out.println("My Hometown Is " + hometown);
+ }
+}
